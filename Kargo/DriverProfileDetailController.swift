a56ca9c32d8c5491288a8acf59f85189740ed901
nicatguliyev <nicatguliyev3@gmail.com>
2019-10-10 01:32:43 +0400

    Okay

--- Kargo/DriverProfileDetailController.swift
+++ Kargo/DriverProfileDetailController.swift
@@ -14,6 +14,7 @@ class DriverProfileDetailController: UIViewController {
         super.viewDidLoad()
         
         //xxxxxxxxxxx
+        ///mmmmmmmmmm
 
         // Do any additional setup after loading the view.
     }
