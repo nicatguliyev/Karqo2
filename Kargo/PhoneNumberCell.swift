1735d52d5426b273a04eae0680ad7e69d227db24
nicatguliyev <nicatguliyev3@gmail.com>
2019-11-02 13:45:25 +0400

    PhoneNumberTest

---
+++ Kargo/PhoneNumberCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhoneNumberCell.swift
+//  Kargo
+//
+//  Created by Nicat Guliyev on 10/23/19.
+//  Copyright © 2019 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class PhoneNumberCell: UITableViewCell {
+    
+    @IBOutlet weak var phoneNumberLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code]
+       // print(::::)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
